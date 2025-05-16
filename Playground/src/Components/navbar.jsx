e97080becb9a1b42743b46e017909b6f9dbfc195
Sadaf <sadafara@gmail.com>
2025-05-16 17:52:58 +0530

    sadaf's initial commit

--- Playground/src/Components/navbar.jsx
+++ Playground/src/Components/navbar.jsx
@@ -1,10 +1,31 @@
 import { NavLink } from "react-router-dom"
+import '../styles/nav.css'
+import { BadgePlus } from 'lucide-react';
+import { House } from 'lucide-react';
+import { BellRing } from 'lucide-react';
+import { Compass } from 'lucide-react';
+import { CircleUserRound } from 'lucide-react';
+import { LogIn } from 'lucide-react';
+import { Laugh } from 'lucide-react';
+
+
 
 const Navbar=()=>{
 
     return(
         <>
-            <h1>Navbar</h1>
+            <div className="nav">
+                 <div className="logo">
+                    <h1><Laugh/> <span className="gradient-text">HeeHeee</span></h1>
+                    <p>Your meme destination</p>
+                </div>
+                <NavLink className='page' to='/'> <House/> Memezz</NavLink>
+                <NavLink className='page' to='/popup'> <BellRing/> Popup</NavLink>
+                <NavLink className='page' to='/create'><BadgePlus/> Create</NavLink>
+                <NavLink className='page' to='/hexplore'><Compass/>Hexplore</NavLink>
+                <NavLink className='page' to='/profile'><CircleUserRound/> My Profile</NavLink>
+                <button className="button"><NavLink className='navlink' to='/login'><LogIn/> Login</NavLink></button>
+            </div>
         </>
     )
 }
